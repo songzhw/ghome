6dcf6affde8c231289e129a41501e92a53be4f75
songzhw <songzhw2012@gmail.com>
2018-12-06 21:09:25 -0500

    personlized resp with ur name

--- codelab2/functions/index.js
+++ codelab2/functions/index.js
@@ -1,20 +1,50 @@
-'use strict';
+"use strict";
 
 // Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+const { dialogflow, Permission, Suggestions } = require("actions-on-google");
 
 // Import the firebase-functions package for deployment.
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // Instantiate the Dialogflow client.
-const app = dialogflow({debug: true});
+const app = dialogflow({ debug: true });
+
+// Handle the Dialogflow intent named 'Default Welcome Intent'.
+app.intent("Default Welcome Intent", conv => {
+  // Asks the user's permission to know their name, for personalization.
+  conv.ask(
+    new Permission({
+      context: "Hi there, to get to know you better",
+      permissions: "NAME"
+    })
+  );
+});
+
+// Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
+// agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
+app.intent("actions_intent_PERMISSION", (conv, params, permissionGranted) => {
+  if (!permissionGranted) {
+    // If the user denied our request, go ahead with the conversation.
+    conv.ask(`OK, no worries. What's your favorite color?`);
+    conv.ask(new Suggestions("Blue", "Red", "Green"));
+  } else {
+    // If the user accepted our request, store their name in
+    // the 'conv.data' object for the duration of the conversation.
+    conv.data.userName = conv.user.name.display;
+    conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
+    conv.ask(new Suggestions("Blue", "Red", "Green"));
+  }
+});
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
-app.intent('favorite color', (conv, {color}) => {
-    const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your second lucky number is ' + luckyNumber);
+app.intent("favorite color", (conv, { color }) => {
+  const luckyNumber = color.length;
+  if (conv.data.userName) {
+    conv.close(`${conv.data.userName}, your lucky nmae is ${luckyNumber}`);
+  } else {
+    conv.close("Your second lucky number is " + luckyNumber);
+  }
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
