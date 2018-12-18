b83484dcfc81268aaf98b7ded103e39628fe7c3c
songzhw <songzhw2012@gmail.com>
2018-12-18 09:35:53 -0500

    however, cannot paly the music

--- homeOne/functions/index.js
+++ homeOne/functions/index.js
@@ -1,9 +1,12 @@
-const { dialogflow } = require("actions-on-google");
+const { dialogflow, Suggestions } = require("actions-on-google");
 const functions = require("firebase-functions");
 const app = dialogflow({ debug: true });
 
 app.intent('manu music', (conv, {playbackOne}) => {
-    conv.close(`here is the playback ${playbackOne}`)
+    let audioSrc = "http://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3"
+    var resultStr = `<speak>music ${playbackOne}: <audio src=${audioSrc}/> </speak>`;
+    conv.ask(resultStr)
+    conv.ask(new Suggestions('Got it', "No, thanks"))
 })
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
