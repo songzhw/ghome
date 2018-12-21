ab35890ad1f0ac4249f7a11db7a01b047045e3cd
songzhw <songzhw2012@gmail.com>
2018-12-21 10:57:52 -0500

    try to close it does not work for me too

--- homeOne/functions/index.js
+++ homeOne/functions/index.js
@@ -2,30 +2,32 @@ const { dialogflow, Suggestions, MediaObject, Image } = require("actions-on-goog
 const functions = require("firebase-functions");
 const app = dialogflow({ debug: true });
 
-// app.intent('manu music', (conv, {playbackOne}) => {
-//   if (!conv.surface.capabilities.has('actions.capability.MEDIA_RESPONSE_AUDIO')) {
-//     conv.ask('Sorry, this device does not support audio playback.');
-//     return;
-//   }
-//   conv.ask(`okay, let's ${playbackOne}`)
-//   conv.ask(new MediaObject({
-//     name: 'Jazz in Paris',
-//     url: 'https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3',
-//     description: 'A funky Jazz tune',
-//     icon: new Image({
-//       url: 'https://storage.googleapis.com/automotive-media/album_art.jpg',
-//       alt: 'Ocean view',
-//     }),
-//   }));
-// })
-
-app.intent('manu music', (conv, {playbackOne, company}) => {
-    // let audioSrc = "https://s1.vocaroo.com/media/download_temp/Vocaroo_s1aKfXwJUaLz.mp3"  // music: < 120s
-    let audioSrc = "https://s1.vocaroo.com/media/download_temp/Vocaroo_s12PH97aukdO.mp3" // audiobook : > 120s. And <speak> plays the whole 3:06s
-let resultStr = `<speak>${playbackOne} on ${company}<audio src="${audioSrc}"/> </speak>`;
-    conv.ask(resultStr)
-    conv.ask(new Suggestions('Got it', "No, thanks"))
+app.intent('manu music', (conv, {playbackOne}) => {
+  if (!conv.surface.capabilities.has('actions.capability.MEDIA_RESPONSE_AUDIO')) {
+    conv.ask('Sorry, this device does not support audio playback.');
+    return;
+  }
+  conv.ask(`okay, let's ${playbackOne}`)
+  conv.ask(new MediaObject({
+    name: 'Jazz in Paris',
+    url: 'https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3',
+    description: 'A funky Jazz tune',
+    icon: new Image({
+      url: 'https://storage.googleapis.com/automotive-media/album_art.jpg',
+      alt: 'Ocean view',
+    }),
+  }));
+  app.ask(new Suggestions("Yes","No"))
+//   app.close('this is the end')
 })
+
+// app.intent('manu music', (conv, {playbackOne, company}) => {
+//     // let audioSrc = "https://s1.vocaroo.com/media/download_temp/Vocaroo_s1aKfXwJUaLz.mp3"  // music: < 120s
+//     let audioSrc = "https://s1.vocaroo.com/media/download_temp/Vocaroo_s12PH97aukdO.mp3" // audiobook : > 120s. And <speak> plays the whole 3:06s
+// let resultStr = `<speak>${playbackOne} on ${company}<audio src="${audioSrc}"/> </speak>`;
+//     conv.ask(resultStr)
+//     conv.ask(new Suggestions('Got it', "No, thanks"))
+// })
 
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
