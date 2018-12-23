996dbb2d7c8ba7c16e406a0a731cd83c4d8ffda6
songzhw <songzhw2012@gmail.com>
2018-12-23 15:34:50 -0500

    refactor according to "https://www.eisbahn.jp/yoichiro/2018/06/media_responses_en.html"

--- homeOne/functions/index.js
+++ homeOne/functions/index.js
@@ -15,23 +15,22 @@ app.intent("manu music", (conv, { playbackOne, company }) => {
     conv.ask("Sorry, this device does not support audio playback.");
     return;
   }
-  conv.ask(`okay, version 5, let's ${playbackOne} on ${company}`);
+  conv.ask(`okay, version 6, let's ${playbackOne} on ${company}`);
 
-  const offset = conv.user.storage.offset;
-  if (!offset) {
-    conv.user.storage.offset = 23;
-    conv.ask("no offset cached ");
-  } else {
-    conv.ask(`cached offset is ${offset}`);
-  }
+  // const offset = conv.user.storage.offset;
+  // if (!offset) {
+  //   conv.user.storage.offset = 23;
+  //   conv.ask("no offset cached ");
+  // } else {
+  //   conv.ask(`cached offset is ${offset}`);
+  // }
 
   conv.ask(new SimpleResponse("I am Simple Reponse"));
-  conv.ask(new Suggestions("Yes", "No"));
-  conv.close(
+
+  conv.ask(
     new MediaObject({
       name: "Jazz in Paris",
-      url:
-        "https://s3.ca-central-1.amazonaws.com/test-audiobooks/sample.mp3 ",
+      url: "https://s3.ca-central-1.amazonaws.com/test-audiobooks/sample.mp3 ",
       description: "A funky Jazz tune",
       icon: new Image({
         url: "https://storage.googleapis.com/automotive-media/album_art.jpg",
@@ -39,6 +38,8 @@ app.intent("manu music", (conv, { playbackOne, company }) => {
       })
     })
   );
+
+  conv.ask(new Suggestions("Yes", "No"));
 });
 // https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3  : streaming type, 01:42 long
 // https://s1.vocaroo.com/media/download_temp/Vocaroo_s1cJCrKVFdol.mp3: streaming type, 15:05 long
