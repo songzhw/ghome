62bfaeb028e00d5e1048773b4256c4dd1dba8dd6
songzhw <songzhw2012@gmail.com>
2018-12-24 21:28:37 -0500

    add the webhook code. However, it's only say "playback status = ***" when the song is finished.

--- homeOne/functions/index.js
+++ homeOne/functions/index.js
@@ -37,8 +37,14 @@ app.intent("manu music", (conv, { playbackOne }) => {
   conv.ask(new Suggestions("Yes", "No"));
   });
 
+  // It seems it only says the word when the playing is finished
 app.intent('MEDIA_STATUS', (conv) => {
-
+  const mediaStatus = conv.arguments.get("MEDIA_STATUS");
+  if (mediaStatus) {
+    conv.ask(`playback status = ${mediaStatus.status}`);
+  } else {
+    conv.ask("empty status ");
+  }
 });
 // https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3  : streaming type, 01:42 long
 // https://s1.vocaroo.com/media/download_temp/Vocaroo_s1cJCrKVFdol.mp3: streaming type, 15:05 long
