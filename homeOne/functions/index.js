35c6c479aed84b64e32bcf95cf3daf7410949836
songzhw <songzhw2012@gmail.com>
2018-12-26 12:56:12 -0500

    saved the offset; still two issues 1) "offset - 13", this 13 is a magic number, it could change when you are playing different books 2) setInternal() method is not okay for the "ok google, skip forward 10s"

--- homeOne/functions/index.js
+++ homeOne/functions/index.js
@@ -8,11 +8,13 @@ const {
 const functions = require("firebase-functions");
 const app = dialogflow({ debug: true });
 
-const version = 20;
+const version = 24;
+
 var offset = 0;
-var intervalId = 0
+var intervalId = 0;
+
 app.intent("Default Welcome Intent", conv => {
-  let offset = conv.user.storage.offset;
+  offset = conv.user.storage.offset;
   let start = `version ${version}. Welcome`;
   if (offset) {
     conv.ask(`${start}. the offset we just saved is ${offset}`);
@@ -21,59 +23,21 @@ app.intent("Default Welcome Intent", conv => {
   }
 });
 
-// app.intent("manu music", (conv, { playbackOne }) => {
-//   conv.ask(new SimpleResponse(`version ${version}`));
-
-//   conv.ask(
-//     new MediaObject({
-//       name: "Jazz in Paris",
-//       url: "https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3",
-//       description: "A funky Jazz tune",
-//       icon: new Image({
-//         url: "https://storage.googleapis.com/automotive-media/album_art.jpg",
-//         alt: "Ocean view"
-//       })
-//     })
-//   );
-
-//   conv.ask(new Suggestions("Yes", "No"));
-// });
-// https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3  : streaming type, 01:42 long
-// https://s1.vocaroo.com/media/download_temp/Vocaroo_s1cJCrKVFdol.mp3: streaming type, 15:05 long
-// https://s3.ca-central-1.amazonaws.com/test-audiobooks/sample.mp3 : streaming type, 1:12:55
-
-
-
 app.intent("manu music", (conv, { playbackOne, company }) => {
   let audioSrc = "https://s3.ca-central-1.amazonaws.com/test-audiobooks/sample.mp3";
-  let resultStr = `<speak>${playbackOne} on ${company}<audio src="${audioSrc}" clipBegin="100s"/> </speak>`;
+  let from = offset;
+  let resultStr = `<speak>${playbackOne}<audio src="${audioSrc}" clipBegin="${from}s"/> </speak>`;
   conv.ask(resultStr);
   conv.ask(new Suggestions("Got it", "No, thanks"));
 
-  intervalId = setInterval( () => offset = offset + 1, 1000)
-});
-
-// It seems it only says the word when the playing is finished
-app.intent("MEDIA_STATUS", conv => {
-  const mediaStatus = conv.arguments.get("MEDIA_STATUS");
-  if (mediaStatus) {
-    conv.ask(`playback status = ${mediaStatus.status}`);
-  } else {
-    conv.ask("empty status ");
-  }
+  intervalId = setInterval(() => (offset = offset + 1), 1000);
 });
 
 app.intent("leave conv", conv => {
-  // TODO get the offset from the playback
-  let savedOffset = Math.floor(Math.random() * 100);
-  conv.user.storage.offset = savedOffset;
-  clearInterval(intervalId)
-  // conv.close(
-  //   `Let me know when you want to resume this audio book : ${
-  //     conv.user.storage.offset
-  //   }, offset = ${offset}`
-  // );
-  conv.close(`offset = ${JSON.stringify(offset)}`)
+  conv.user.storage.offset = offset - 13;
+  clearInterval(intervalId);
+  offset = 0;
+  conv.close(`offset = ${conv.user.storage.offset}`);
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
